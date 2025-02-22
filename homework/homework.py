114b723c13f3ba299c09cf81e0c0911ebcd2d013
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2025-02-22 14:09:23 -0500

    commit
    commit

--- homework/homework.py
+++ homework/homework.py
@@ -61,3 +61,114 @@
 # {'type': 'metrics', 'dataset': 'train', 'r2': 0.8, 'mse': 0.7, 'mad': 0.9}
 # {'type': 'metrics', 'dataset': 'test', 'r2': 0.7, 'mse': 0.6, 'mad': 0.8}
 #
+
+
+import pandas as pd
+import gzip
+import json
+import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error,median_absolute_error
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression
+
+ruta_test = "./files/input/test_data.csv.zip"
+ruta_train = "./files/input/train_data.csv.zip"
+
+df_test = pd.read_csv(
+    ruta_test,
+    index_col=False,
+    compression='zip'
+)
+
+df_train = pd.read_csv(
+    ruta_train,
+    index_col=False,
+    compression='zip'
+)
+
+anio_actual = 2021
+
+df_train['Age'] = anio_actual - df_train['Year']
+df_test['Age'] = anio_actual - df_test['Year']
+
+columnas_eliminar = ['Year', 'Car_Name']
+df_train = df_train.drop(columns=columnas_eliminar)
+df_test = df_test.drop(columns=columnas_eliminar)
+
+entradas_train = df_train.drop(columns="Present_Price")
+salidas_train = df_train["Present_Price"]
+
+entradas_test = df_test.drop(columns="Present_Price")
+salidas_test = df_test["Present_Price"]
+
+caracteristicas_categoricas = ['Fuel_Type', 'Selling_type', 'Transmission']
+caracteristicas_numericas = list(set(entradas_train.columns) - set(caracteristicas_categoricas))
+
+procesador = ColumnTransformer(
+    transformers=[
+        ("num", MinMaxScaler(), caracteristicas_numericas),
+        ("cat", OneHotEncoder(), caracteristicas_categoricas)
+    ],
+    remainder="passthrough"
+)
+
+k_mejor = SelectKBest(f_regression, k='all')
+
+modelo = LinearRegression()
+
+tuberia = Pipeline(
+    steps=[
+        ("preprocessor", procesador),
+        ("k_best", k_mejor),
+        ("model", modelo)
+    ]
+)
+
+rejilla_parametros = {
+    "k_best__k": list(range(1, 16)),
+    "model__fit_intercept": [True, False]
+}
+
+busqueda_grid = GridSearchCV(
+    tuberia,
+    param_grid=rejilla_parametros,
+    cv=10,
+    scoring="neg_mean_absolute_error",
+    n_jobs=-1,
+    refit=True,
+    verbose=1
+)
+
+busqueda_grid.fit(entradas_train, salidas_train)
+
+with gzip.open("./files/models/model.pkl.gz", 'wb') as f:
+    pickle.dump(busqueda_grid, f)
+    
+resultados_metricas = {}
+
+pred_train = busqueda_grid.predict(entradas_train)
+pred_test = busqueda_grid.predict(entradas_test)
+
+resultados_metricas['train'] = {
+    'type': 'metrics',
+    'dataset': 'train',
+    'r2': r2_score(salidas_train, pred_train),
+    'mse': mean_squared_error(salidas_train, pred_train),
+    'mad': median_absolute_error(salidas_train, pred_train),
+}
+
+resultados_metricas['test'] = {
+    'type': 'metrics',
+    'dataset': 'test',
+    'r2': r2_score(salidas_test, pred_test),
+    'mse': mean_squared_error(salidas_test, pred_test),
+    'mad': median_absolute_error(salidas_test, pred_test),
+}
+
+with open("./files/output/metrics.json", 'w') as f:
+    f.write(json.dumps(resultados_metricas['train'])+'\n')
+    f.write(json.dumps(resultados_metricas['test'])+'\n')
